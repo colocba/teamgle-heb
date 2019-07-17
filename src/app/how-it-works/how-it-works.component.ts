ebc3a245a6c63a52e4b1a97a2afafd52aeb07d83
amirbaum <colocba@gmail.com>
2019-07-17 11:37:11 +0300

    Direction problems in hebrew solved

--- src/app/how-it-works/how-it-works.component.ts
+++ src/app/how-it-works/how-it-works.component.ts
@@ -11,24 +11,24 @@ export class HowItWorksComponent implements OnInit {
 
   cards1 = [
     {url: "assets/cloud-syn-logo/cloud-syn-logo.png" ,
-     title: "הכל במקום אחד. מערכת בענן, זמינה ועדכנית מכל מקום למנהלים ולעובדים"},
+     title: "הכל במקום אחד. מערכת בענן, זמינה ועדכנית מכל מקום למנהלים ולעובדים."},
     {url: "assets/anounce-shifts-logo/anounce-shift-logo.png" ,
-     title: "פרסמו את האירועים שלכם והמשמרות הפתוחות"},
+     title: "פרסמו את האירועים שלכם והמשמרות הפתוחות."},
     {url: "assets/choose-shifts-logo/choose-shifts-logo.png" ,
-     title: "אפשרו לצוות לבחור משמרות. כל אחד לפי ההרשאה שלו"},
+     title: "אפשרו לצוות לבחור משמרות. כל אחד לפי ההרשאה שלו."},
     {url: "assets/update-changes-logo/update-changes-logo.png" ,
-     title: "עדכנו בשינויים של הרגע האחרון. הודעות מייל, הודעות טקסט והודעות פוש"}
+     title: "עדכנו בשינויים של הרגע האחרון. הודעות מייל, הודעות טקסט והודעות פוש."}
   ]
 
   cards2 = [
     {url: "assets/monitor-scheddule-logo/monitor-scheddule-logo.png" ,
-     title: "אפשרו לצוות לצפות בסידור עבודה ובתלוש שכר"},
+     title: "אפשרו לצוות לצפות בסידור עבודה ובתלוש שכר."},
     {url: "assets/logistics-mgmnt-logo/logistics-mgmnt-logo.png" ,
-     title: "נהלו ציוד ולוגיסטיקה"},
+     title: "נהלו ציוד ולוגיסטיקה."},
     {url: "assets/data-payment-system-logo/data-payment-system-logo.png" ,
-     title: "יצאו נתונים למערכת השכר"},
+     title: "יצאו נתונים למערכת השכר."},
     {url: "assets/worker-history-logo/worker-history-logo.png" ,
-     title: "שמירת היסטוריה וניסיון של עובדים"}
+     title: "שמירת היסטוריה וניסיון של עובדים."}
   ]
 
   ngOnInit() {
