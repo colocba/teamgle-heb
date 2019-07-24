7915392e29b470c0a02e1b23edba978f7efc0a3e
amirbaum <colocba@gmail.com>
2019-07-24 12:27:07 +0300

    Making navbar effects on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    // Changing the navbar style on scroll only if we are in desktop mode
+    if (screen.width > 800) {
+      $(window).scroll(function() {
+        $('nav').toggleClass('scrolled', $(this).scrollTop() > 50)
+        $('.cov-img').toggleClass('scrolled1', $(this).scrollTop() > 50)
+        $('.navbar').toggleClass('downsized', $(this).scrollTop() > 50)
+      })
+    }
+  }
   title = 'teamgle-heb';
 }
